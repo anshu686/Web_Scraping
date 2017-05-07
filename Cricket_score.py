fc0b6849ad69e178e1e6d722a1a3acc1d8d0dc48
PRITESH SRIVASTAVA <anshu.68ec@gmail.com>
2017-05-07 19:42:34 +0530

    Desktop notifier for live cricket scores
    Scraped the official website of http://www.espncricinfo.com/ and fetched the live scores in order to display it as desktop notification.

---
+++ Cricket_score.py
@@ -0,0 +1,46 @@
+import feedparser
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import notify2
+import os, time, random, re
+
+
+def Parsefeed():
+
+    while True:
+
+        time.sleep(random.uniform(6, 10))
+        f = urlopen("http://www.espncricinfo.com/ci/engine/match/1082636.html?CMP=OTC-RSS")
+        ICON_PATH = os.getcwd() + "/icon.ico"
+        notify2.init('News Notify')
+        time.sleep(random.uniform(6, 10))
+        soup = BeautifulSoup(f, 'html.parser')
+        # print(soup.prettify())
+
+        j = soup.get_text('class:', 'large-13 medium-13 columns innings-information')
+
+        regex = r".*:"
+        # matches = []
+        matches = re.findall(regex, j)[0]
+        regex2 = r".*\)"
+        title = re.findall(r"\|.*\|", matches)[0]
+        score = re.findall(regex2, matches)[0]
+
+        # print(title)
+        # print(score)
+
+
+
+        n = notify2.Notification(title,
+                                 score,
+                                 icon=ICON_PATH
+                                 )
+        #
+        n.set_urgency(notify2.URGENCY_NORMAL)
+        n.show()
+        n.set_timeout(20)
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    Parsefeed()
